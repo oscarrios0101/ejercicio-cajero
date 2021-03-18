ed61d6a0c553719e7a46bcf2092dbeab028af6b6
Oscar <oscarrios0101@gmail.com>
2021-03-18 15:17:30 -0700

    validacion basica con varios usuarios con json lograda

--- app.js
+++ app.js
@@ -3,19 +3,12 @@ const modal_container = document.querySelector('.modal-container');
 const form = document.querySelector('#form');
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
+const modal_message = document.querySelector('.modal_message');
 
 close.addEventListener('click', function () {
   modal_container.style.opacity = '0';
   modal_container.style['pointer-events'] = 'none';
 });
-
-function evalPassword(obj) {
-  console.log(obj.password);
-  console.log('el password por el usuario es', password.value);
-  if (obj.password == password.value) {
-    console.log('oscar inicio secion');
-  }
-}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault(e);
@@ -28,3 +21,21 @@ form.addEventListener('submit', (e) => {
       evalPassword(obj);
     });
 });
+
+function evalPassword(obj) {
+  obj.accounts.forEach((element) => {
+    if (
+      element.password == password.value &&
+      element.username == username.value
+    ) {
+      console.log(`el usuario ${element.username} tiene acceso`);
+      modal_container.style.opacity = '1';
+      modal_container.style['pointer-events'] = 'auto';
+      modal_message.innerHTML = 'Ahora tienes Acceso a mi onlyfans';
+    } else {
+      modal_container.style.opacity = '1';
+      modal_container.style['pointer-events'] = 'auto';
+      modal_message.innerHTML = 'Por Favor Ingresa Una Contraseña valida';
+    }
+  });
+}
